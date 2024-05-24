855668b0dfc5077c468b32b273903a97919278d8
Jakhon <example@gmail.com>
2024-05-24 10:52:15 +0900

    All pages

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,26 @@
+import { blogs } from '@/app/constants'
+import BlockCard from '@/components/cards/block-card'
+import BgArrow from '@/components/shared/bg-arrow'
+
 function HomePage() {
-	return <div>HomePage</div>
+	return (
+		<div className='max-w-6xl mx-auto'>
+			<div className='relative min-h-[60vh] flex items-center justify-center'>
+				<h1 className='text-3xl md:text-4xl lg:text-5xl font-creteRound  text-center max-w-2xl'>
+					Taking control of your daily life is easy when you know how!
+				</h1>
+				<BgArrow />
+			</div>
+			<h2 className='text-4xl text-center section-title font-creteRound'>
+				<span>Recent Posts</span>
+			</h2>
+			<div className='flex flex-col space-y-24 mt-24'>
+				{blogs.map(blog => (
+					<BlockCard key={blog.title} {...blog} />
+				))}
+			</div>
+		</div>
+	)
 }
 
 export default HomePage
--- app/types/index.ts
+++ app/types/index.ts
@@ -1,3 +1,16 @@
 export interface ChildProps {
 	children: React.ReactNode
 }
+export interface IBlog {
+	title: string
+	description: string
+	author: string
+	date: string
+	image: string
+	tags: string[]
+}
+
+export interface IAuthor {
+	name: string
+	image: string
+}
